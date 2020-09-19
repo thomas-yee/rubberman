85ec1fdcf3cedbdfce0e5279184490c9f46b49e2
firestar555 <thomasyee40@gmail.com>
2020-09-19 14:22:17 -0600

    Created basic pages for about and contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Rubberman</h1>
+      <p>
+        Where to contact us <Link to="/contact">Contact us</Link>
+      </p>
+    </div>
+  )
 }
+export default IndexPage
