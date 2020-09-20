45c32759107c1d15ab4625e91b9056ad4ff441c7
firestar555 <thomasyee40@gmail.com>
2020-09-20 16:03:52 -0600

    Added title and paragraph for our story

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,15 @@ module.exports = {
   /* Your site config here */
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,34 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import BackgroundSection from "../components/backgroundSection"
+import Info from "../components/Home/info"
 
-const IndexPage = () => {
+const IndexPage = props => {
   return (
     <div>
-      <Layout>HELLO</Layout>
+      <Layout>
+        <BackgroundSection
+          img={props.data.image.childImageSharp.fluid}
+          title=""
+          styleClass="default-background"
+        />
+        <Info />
+      </Layout>
     </div>
   )
 }
+
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    image: file(relativePath: { eq: "Rubberman-logo-new1.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
