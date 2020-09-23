0b20c2e29c49849ef1b4172be53dd011147e375d
firestar555 <thomasyee40@gmail.com>
2020-09-22 20:45:13 -0600

    added title to info section

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-import Title from "../components/title"
+import Title from "../title"
 
 const Info = () => {
   return (
     <section className="py-5">
       <div className="container">
-        <Title title="our story" />
+        <Title title="Our Story" />
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
--- src/components/title.js
+++ src/components/title.js
@@ -1,9 +1,13 @@
 import React from "react"
 
-const Title = title => {
+const Title = ({ title }) => {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="row">
+      <div className="col text-center mb-3">
+        <h1 className="display-4 text-capitalize font-weight-bold">{title}</h1>
+      </div>
     </div>
   )
 }
+
+export default Title
