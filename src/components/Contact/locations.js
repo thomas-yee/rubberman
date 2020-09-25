81a018ea206769c72b10a92b08cb68d3ca00e5ba
firestar555 <thomasyee40@gmail.com>
2020-09-24 22:50:03 -0600

    Created locations section for contact page

---
+++ src/components/Contact/locations.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Locations = () => {
+  return (
+    <section className="py-5">
+      <div>Office Locations</div>
+    </section>
+  )
+}
+
+export default Locations
