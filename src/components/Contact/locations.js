4c1ad49e5a099502d818588c0becc0a77357e73f
firestar555 <thomasyee40@gmail.com>
2020-09-25 17:51:21 -0600

    Added a location placement

--- src/components/Contact/locations.js
+++ src/components/Contact/locations.js
@@ -1,9 +1,24 @@
 import React from "react"
+import Title from "../title"
 
 const Locations = () => {
   return (
     <section className="py-5">
-      <div>Office Locations</div>
+      <div className="container">
+        <Title title="Office Locations" />
+
+        <div className="row">
+          <div className="col">
+            <h4 className="font-weight-bold">Calgary </h4>
+            <text>
+              2000 Street NW <br />
+              Calgary, AB, 3K6 L0D <br />
+              Canada <br />
+            </text>
+          </div>
+        </div>
+        <div className="row"></div>
+      </div>
     </section>
   )
 }
