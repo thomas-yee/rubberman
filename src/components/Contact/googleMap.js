ebd96610bcc85083ded1ce687bef26497520cc78
firestar555 <thomasyee40@gmail.com>
2020-09-27 22:26:12 -0600

    Implemented google maps to show the office location

---
+++ src/components/Contact/googleMap.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Map from "google-map-react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"
+
+const APIKEY = "AIzaSyC_3Lk9znbSQICmGjqXO5YEIhTPnOWCVAc"
+
+const AnyReactComponent = () => (
+  <div>
+    <FontAwesomeIcon icon={faMapMarkerAlt} className="location-icon fa-3x" />
+  </div>
+)
+
+const GoogleMap = props => {
+  return (
+    <div
+      style={{
+        height: props.location.style.height,
+        width: props.location.style.width,
+      }}
+    >
+      <Map
+        bootstrapURLKeys={{ key: APIKEY }}
+        defaultZoom={props.location.defaultZoom}
+        defaultCenter={{
+          lat: props.location.defaultCenter.lat,
+          lng: props.location.defaultCenter.lng,
+        }}
+      >
+        <AnyReactComponent
+          lat={props.location.defaultCenter.lat}
+          lng={props.location.defaultCenter.lng}
+          text="my marker"
+        />
+      </Map>
+    </div>
+  )
+}
+
+export default GoogleMap
--- src/components/Contact/locations.js
+++ src/components/Contact/locations.js
@@ -2,8 +2,24 @@ import React from "react"
 import Title from "../title"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLocationArrow } from "@fortawesome/free-solid-svg-icons"
+import Map from "./googleMap"
 
 import "./contact.css"
+
+const locations = [
+  {
+    name: "location1",
+    defaultZoom: 15,
+    defaultCenter: {
+      lat: 50.414361,
+      lng: -105.531109,
+    },
+    style: {
+      height: "40vh",
+      width: "85%",
+    },
+  },
+]
 
 const Locations = () => {
   return (
@@ -12,19 +28,22 @@ const Locations = () => {
         <Title title="Office Locations" />
 
         <div className="row">
-          <div className="col">
+          <div className="col-1 pt-4">
             <FontAwesomeIcon
               icon={faLocationArrow}
               className="location-icon fa-3x"
             />
           </div>
-          <div className="col-11">
+          <div className="col-3">
             <h4 className="font-weight-bold">Calgary </h4>
             <text>
               2000 Street NW <br />
               Calgary, AB, 3K6 L0D <br />
               Canada <br />
             </text>
+          </div>
+          <div className="col">
+            <Map location={locations[0]} />
           </div>
         </div>
         <div className="row"></div>
