8b8c89410f27cfe9d2de9eeee2560de795d851b2
firestar555 <thomasyee40@gmail.com>
2020-10-13 17:31:59 -0600

    Pass the language found in the local storage to the pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import Info from "../components/Home/info"
 import Intro from "../components/Home/intro"
 import LogoSection from "../components/Home/logoSection"
 
 const IndexPage = () => {
+  const languageStoredInLocalStorage = localStorage.getItem("language")
+
   return (
     <div>
       <Layout>
-        <Intro />
-        <Info />
-        <LogoSection />
+        <Intro language={languageStoredInLocalStorage} />
+        <Info language={languageStoredInLocalStorage} />
+        <LogoSection language={languageStoredInLocalStorage} />
       </Layout>
     </div>
   )
