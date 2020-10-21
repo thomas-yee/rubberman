43513ffc60d77ec2c014427bfbf26a8903fc9186
firestar555 <thomasyee40@gmail.com>
2020-10-21 17:40:43 -0600

    Refactored info page

--- src/components/About/info.js
+++ src/components/About/info.js
@@ -6,12 +6,14 @@ const Info = props => {
   let content = {
     English: {
       mainSection: {
+        title: "About Rubberman",
         information:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
       },
     },
     Indonesian: {
       mainSection: {
+        title: "XXXX",
         information: "XXX",
       },
     },
@@ -26,13 +28,15 @@ const Info = props => {
     <section className="py-5 bg-light">
       {checkLanguage()}
       <div className="container">
-        <Title title="Our Story" />
         <div className="row">
-          <div className="col-10 col-sm-8 mx-auto text-center">
+          <div className="col-10 col-sm-8 mx-auto">
+            <h4>{content.mainSection.title}</h4>
+            <hr className="mt-0 mb-2 horizontal-bar" />
             <p className="lead text-muted mb-5">
               {content.mainSection.information}
             </p>
           </div>
+          <div className="col"></div>
         </div>
       </div>
     </section>
