de512f92f5b65a4e724a9347ec92a9e30ff95fdd
firestar555 <thomasyee40@gmail.com>
2020-10-27 22:55:34 -0600

    Added contact us to products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,6 +1,22 @@
 import React from "react"
 import Layout from "../components/layout"
 import Info from "../components/Products/info"
+import ContactUs from "../components/Global/contactUs"
+
+let contactContent = {
+  English: {
+    mainSection: {
+      information: "Still have questions? Get in touch with us",
+      contactLink: "Contact Us",
+    },
+  },
+  Indonesian: {
+    mainSection: {
+      information: "XXXX",
+      contactLink: "XXXX",
+    },
+  },
+}
 
 const ProductsPage = () => {
   const languageStoredInLocalStorage = localStorage.getItem("language")
@@ -8,6 +24,10 @@ const ProductsPage = () => {
     <div>
       <Layout>
         <Info language={languageStoredInLocalStorage} />
+        <ContactUs
+          language={languageStoredInLocalStorage}
+          content={contactContent}
+        />
       </Layout>
     </div>
   )
