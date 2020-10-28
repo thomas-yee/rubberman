ef33a0d37f6bb5c6ca1e20a2b17b05311e28358c
firestar555 <thomasyee40@gmail.com>
2020-10-27 22:52:24 -0600

    added contact section to the page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,34 @@
 import React from "react"
 import Layout from "../components/layout"
 import Info from "../components/About/info"
+import ContactUs from "../components/Global/contactUs"
 
 const AboutPage = () => {
   const languageStoredInLocalStorage = localStorage.getItem("language")
+
+  let contactContent = {
+    English: {
+      mainSection: {
+        information: "Want to learn more? Get in touch with us",
+        contactLink: "Contact Us",
+      },
+    },
+    Indonesian: {
+      mainSection: {
+        information: "XXXX",
+        contactLink: "XXXX",
+      },
+    },
+  }
+
   return (
     <div>
       <Layout>
         <Info language={languageStoredInLocalStorage} />
+        <ContactUs
+          language={languageStoredInLocalStorage}
+          content={contactContent}
+        />
       </Layout>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,36 @@ import Layout from "../components/layout"
 import Info from "../components/Home/info"
 import Intro from "../components/Home/intro"
 import LogoSection from "../components/Home/logoSection"
-import ContactUs from "../components/Home/contactUs"
+import ContactUs from "../components/Global/contactUs"
 
 const IndexPage = () => {
   const languageStoredInLocalStorage = localStorage.getItem("language")
 
+  let contactContent = {
+    English: {
+      mainSection: {
+        information:
+          "Interested in partnering with us? Become part of the Rubberman team!",
+        contactLink: "Contact Us",
+      },
+    },
+    Indonesian: {
+      mainSection: {
+        information: "XXXX",
+        contactLink: "XXXX",
+      },
+    },
+  }
   return (
     <div>
       <Layout>
         <Intro language={languageStoredInLocalStorage} />
         <Info language={languageStoredInLocalStorage} />
         <LogoSection language={languageStoredInLocalStorage} />
-        <ContactUs language={languageStoredInLocalStorage} />
+        <ContactUs
+          language={languageStoredInLocalStorage}
+          content={contactContent}
+        />
       </Layout>
     </div>
   )
