975ecf6256e4b2b9dc41e7401880d56e1f25413b
firestar555 <thomasyee40@gmail.com>
2020-11-09 14:47:59 -0600

    Added more locations

--- src/components/Contact/locations.js
+++ src/components/Contact/locations.js
@@ -7,18 +7,50 @@ const locations = [
   {
     location1: {
       id: 1,
-      name: "location1",
+      name: "Medan",
       coordinates: {
-        lat: -4.391102,
-        lng: 122.085345,
+        lat: 3.595334,
+        lng: 98.673602,
       },
     },
     location2: {
       id: 2,
-      name: "location2",
+      name: "Pematang Siantar",
       coordinates: {
-        lat: -5.293221,
-        lng: 122.861457,
+        lat: 2.955764,
+        lng: 99.063064,
+      },
+    },
+    location3: {
+      id: 3,
+      name: "Padang",
+      coordinates: {
+        lat: -0.905404,
+        lng: 100.386805,
+      },
+    },
+    location4: {
+      id: 4,
+      name: "Pekanbaru",
+      coordinates: {
+        lat: 0.508298,
+        lng: 101.446324,
+      },
+    },
+    location5: {
+      id: 3,
+      name: "Bandar Lampung",
+      coordinates: {
+        lat: -5.394147,
+        lng: 105.26593,
+      },
+    },
+    location6: {
+      id: 4,
+      name: "Serang",
+      coordinates: {
+        lat: -6.118078,
+        lng: 106.159277,
       },
     },
   },
@@ -27,12 +59,12 @@ const locations = [
 const locationSettings = [
   {
     style: {
-      height: "40vh",
+      height: "30vh",
       width: "100%",
     },
     options: {
-      minZoom: 3,
-      maxZoom: 15,
+      minZoom: 3.7,
+      maxZoom: 8,
       defaultCenter: {
         lat: -4.391102,
         lng: 122.085345,
