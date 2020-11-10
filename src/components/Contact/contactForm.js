f51233141fa3fbd8d72469fbdc8aaec92ccadf6a
firestar555 <thomasyee40@gmail.com>
2020-11-10 14:46:23 -0600

    Changed layout of the contact form

--- src/components/Contact/contactForm.js
+++ src/components/Contact/contactForm.js
@@ -4,16 +4,17 @@ import Title from "../title"
 const ContactForm = props => {
   let content = {
     English: {
-      intro:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor",
-      firstName: "First Name",
-      lastName: "Last Name",
-      emailAddress: "Email Address",
+      title: "Let's Talk",
+      intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      firstName: "First Name*",
+      lastName: "Last Name*",
+      emailAddress: "Email*",
       phone: "Phone",
-      message: "Message",
-      button: "Send Message",
+      message: "Message*",
+      button: "Submit",
     },
     Indonesian: {
+      title: "Let's Talk",
       intro: "XXX",
       firstName: "XXX",
       lastName: "XXX",
@@ -31,12 +32,16 @@ const ContactForm = props => {
   }
 
   return (
-    <section className="py-5 bg-light">
+    <section className="py-5">
       {checkLanguage()}
       <div className="container">
-        <Title title="Let's Talk" />
         <div className="row">
-          <div className="col-10 col-sm-8 mx-auto text-center">
+          <div className="col-10 col-sm-8 mb-2">
+            <h3>{content.title}</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-10 col-sm-8">
             <p className="lead text-muted mb-5">{content.intro}</p>
           </div>
         </div>
@@ -44,58 +49,56 @@ const ContactForm = props => {
           <div className="form-row">
             {/*name*/}
             <div className="form-group col-10 col-sm-8 col-md-6">
-              <label htmlFor="name">{content.firstName}</label>
               <input
                 type="text"
                 className="form-control"
                 name="name"
                 id="name"
+                placeholder={content.firstName}
               ></input>
             </div>
             <div className="form-group col-10 col-sm-8 col-md-6">
-              <label htmlFor="name">{content.lastName}</label>
               <input
                 type="text"
                 className="form-control"
                 name="name"
                 id="name"
+                placeholder={content.lastName}
               ></input>
             </div>
           </div>
           <div className="form-row">
             {/*Email*/}
             <div className="form-group col-10 col-sm-8 col-md-6">
-              <label htmlFor="email">{content.emailAddress}</label>
               <input
                 type="text"
                 className="form-control"
                 name="email"
                 id="email"
-                placeholder="you@example.com"
+                placeholder={content.emailAddress}
               ></input>
             </div>
             {/*Phone number*/}
             <div className="form-group col-10 col-sm-8 col-md-6">
-              <label htmlFor="phone">{content.phone}</label>
               <input
                 type="text"
                 className="form-control"
                 name="phone"
                 id="phone"
-                placeholder="(000)-203-2039"
+                placeholder={content.phone}
               ></input>
             </div>
           </div>
           <div className="form-group">
             {/*Message*/}
             <div className="form-group">
-              <label htmlFor="message">{content.message}</label>
               <textarea
                 type="text"
                 className="form-control"
                 name="message"
                 id="message"
                 rows="4"
+                placeholder={content.message}
               ></textarea>
             </div>
           </div>
